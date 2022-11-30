ea51ed95913b210dcc20dc983c0df003417ac1ad
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-11-30 16:53:57 -0600

    Deployment

--- Backend/src/db/connection.js
+++ Backend/src/db/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const db = new Sequelize(process.env.DB_NAME || 'SocialHub', process.env.DB_USERNAME || 'postgres', process.env.DB_PASSWORD || '12345678', {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '5432',
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
